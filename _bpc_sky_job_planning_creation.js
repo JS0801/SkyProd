d191836281ec86b7efd96db001b3eb94c6fc46f0
JS0801 <jsuthar@oncloudconsulting.ca>
2026-05-12 10:09:22 -0400

    Update from NetSuite (Auto)

--- _bpc_sky_job_planning_creation.js
+++ _bpc_sky_job_planning_creation.js
@@ -358,7 +358,7 @@ define(['N/record', 'N/search', 'N/log'], function (record, search, log) {
                             search.createColumn({
                                 name: "formulatext",
                                 summary: "GROUP",
-                                formula: "CASE   WHEN {custrecord_bpc_planning_job.custrecord_bpc_plandetail_print_method} IN   ('Traditional', 'Traditional : Screen Print', 'Traditional : Pad Print', 'Hi-Speed')   THEN 'Regular' WHEN {custrecord_bpc_planning_job.custrecord_bpc_plandetail_print_method} = 'Digital '   THEN 'CUT'  WHEN {custrecord_bpc_planning_job.custrecord_bpc_plandetail_print_method} = 'Traditional : Hot Stamp'   THEN 'HO'  WHEN {custrecord_bpc_planning_job.custrecord_bpc_plandetail_print_method} = 'Traditional : Emboss'   THEN 'EM'  WHEN {custrecord_bpc_planning_job.custrecord_bpc_plandetail_print_method} = 'Traditional : Deboss'   THEN 'DE'END",
+                                formula: "CASE  WHEN {custrecord_bpc_planning_job.custrecord_bpc_planning_op_name} LIKE '%Run%' THEN 1 ELSE 0 END",
                                 label: "Machine"
                             }),
                             // search.createColumn({
